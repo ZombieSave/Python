40d9f7e02fa1a1ee980ac6864f138a76eca05e1a
mars <you@example.com>
2021-04-10 14:21:54 +0300

    Задача 1

---
+++ Урок1.Задача1.py
@@ -0,0 +1,19 @@
+n = 10
+f = 11.23
+s = 'String'
+b = True
+
+print(n)
+print(f)
+print(s)
+print(b)
+print(n, f, s, b)
+
+num = input("Введите целое число: ")
+print("Введено число " + str(int(num)))
+
+fl = input("Введите дробное число с точкой в виде разделителя дробной части: ")
+print("Введено число " + str(float(fl)))
+
+st = input("Введите строку: ")
+print("Введена строка '" + st + "'")
