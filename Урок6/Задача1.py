8b7e16129c41a2f0432f2dab3d1507937087ee06
mars <you@example.com>
2021-05-01 02:01:08 +0300

    1

--- Урок6/Задача1.py
+++ Урок6/Задача1.py
@@ -3,26 +3,17 @@ import time
 
 class TrafficLight:
     def __init__(self):
-        self.__color = [31, 33, 32]
+        self.__color = [(31, 7), (33, 2), (32, 3)]
 
     def running(self):
         i = 0
-        k = 1
+        k = -1
 
         while True:
-            print(f"\033[7m\033[{self.__color[i]}m")
+            print(f"\033[7m\033[{self.__color[i][0]}m")
+            time.sleep(self.__color[i][1])
 
-            if i == 0:
-                time.sleep(7)
-                k = 1
-            elif i == 1:
-                time.sleep(2)
-            elif i == 2:
-                time.sleep(3)
-                k = -1
-
+            if i == 0 or i == len(self.__color) - 1:
+                k *= -1
+                
             i += k
-
-
-tl = TrafficLight()
-tl.running()
